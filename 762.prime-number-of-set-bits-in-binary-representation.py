281cf5c5e2ef0d51902f5d9aff34dcfe5fd51449
hsingko <hsingko@protonmail.com>
2022-05-19 21:53:59 +0800

    easy

---
+++ 762.prime-number-of-set-bits-in-binary-representation.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=762 lang=python3
+#
+# [762] Prime Number of Set Bits in Binary Representation
+#
+
+# @lc code=start
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        
+# @lc code=end
+
--- 836.rectangle-overlap.py
+++ 836.rectangle-overlap.py
@@ -7,6 +7,9 @@
 # @lc code=start
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        verts1 = [(x,y) for x in [rec1[0],rec1[2]] for y in [rec1[1], rec1[3]]]
+        verts2 = [(x,y) for x in [rec2[0],rec2[2]] for y in [rec2[1], rec2[3]]]
         
+        return any(map(is_in_rec2, verts))
 # @lc code=end
 
